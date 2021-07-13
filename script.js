b59eb33d70dec595df3bad0dc71dc142a5c6534a
Cristina.Alekseeva <cristina.alekseeva@gmail.com>
2021-07-14 01:32:09 +0300

    update menu on bounds change

--- script.js
+++ script.js
@@ -14,113 +14,17 @@ const hoverPreset = {
 function init() {
   let map;
 
-  // Создание экземпляра карты.
-  function createMap(data) {
-    map = new ymaps.Map(
-      'map',
-      {
-        center: [55.753215, 37.622504],
-        zoom: 9,
-        controls: ['zoomControl'],
-      },
-      {
-        maxZoom: 17,
-      }
-    );
-
-    const objectManager = new ymaps.ObjectManager({
-      clusterize: false,
-    });
-
-    const collection = {
-      type: 'FeatureCollection',
-      features: data.map(({ id, coords, vendor }) => ({
-        type: 'Feature',
-        id,
-        geometry: {
-          type: 'Point',
-          coordinates: coords,
-        },
-        options: {
-          ...defaultPreset,
-          iconLayout: 'default#image',
-          iconImageHref: `assets/img/placemark-${getSlug(vendor)}.png`,
-        },
-      })),
-    };
-    objectManager.add(collection);
-    map.geoObjects.add(objectManager);
-
-    map.geoObjects.events.add('click', (event) => {
-      const id = event.get('objectId');
-      createCard(id);
-
-      map.geoObjects.events.add('click', (event) => {
-        if (event.get('objectId') !== id) {
-          objectManager.objects.setObjectOptions(id, { ...defaultPreset });
-        }
-      });
-    });
-
-    //create menu
-    $(function () {
-      data.forEach((shop) => {
-        const menuItem = getCardContent(shop);
-
-        menuItem.bind('mouseover', function () {
-          objectManager.objects.setObjectOptions(shop.id, { ...hoverPreset });
-        });
-        menuItem.bind('mouseout', function () {
-          objectManager.objects.setObjectOptions(shop.id, { ...defaultPreset });
-        });
-        menuItem.appendTo($('.Menu-List'));
-      });
-      $('.Menu-Expansion').append(
-        ` <p>Смотреть в списке <b>${data.length} оффлайн-${pluralizeShop(data.length)}</b></p>`
-      );
-      if (window.innerWidth < 560) {
-        $('.Menu-Item').bind('click', function (event) {
-          const id = event.currentTarget.id.replace('item-', '');
-          createCard(id);
-          $('.Menu').removeClass('Expanded');
-        });
-      }
-    });
-
-    function getCardContent(shop) {
-      const img = $('<img>').attr('src', `assets/img/${getSlug(shop.vendor)}.png`);
-      const image = $('<div>').addClass('Menu-Item-Image').append(img);
-      const vendor = $('<h4>' + shop.vendor + '</h4>');
-      const address = $('<p>' + shop.address + '</p>');
-      const info = $('<div>').addClass('Menu-Item-Info').append(vendor, address);
-      const menuItem = $('<div>').addClass('Menu-Item').attr('id', `item-${shop.id}`).append(image, info);
-      return menuItem;
-    }
-
-    function createCard(id) {
-      const content = $('<div>')
-        .addClass('Map-Modal-Card-Content')
-        .append(getCardContent(data.find((el) => el.id === id)));
-
-      const close = $('<span>')
-        .addClass('close-btn')
-        .bind('click', function () {
-          $('.Map-Modal-Card').css('display', 'none');
-          objectManager.objects.setObjectOptions(id, { ...defaultPreset });
-        });
-      $('.Map-Modal-Card').empty().append(content, close);
-      $('.Map-Modal-Card').css('display', 'block');
-      objectManager.objects.setObjectOptions(id, { ...hoverPreset });
-    }
-
-    $('.Map-Modal').css('display', 'block');
-  }
-
   $('#toggle').bind('click', function () {
     fetch('https://sheetdb.io/api/v1/fxwqiedl4xd7p')
       .then((response) => response.json())
       .then((res) => {
-        const data = res.map((el) => ({
+        const formatted = res.sort((a, b) =>
+          a.address
+            .replace(new RegExp('^([^ ]+ )'), '')
+            .trim()
+            .localeCompare(b.address.replace(new RegExp('^([^ ]+ )'), '').trim())
+        );
+        const data = formatted.map((el) => ({
           id: el.id,
           address: el.address,
           vendor: el.vendor,
@@ -140,17 +44,128 @@ function init() {
   });
 }
 
-function getShops() {
-  fetch('https://sheetdb.io/api/v1/fxwqiedl4xd7p')
-    .then((response) => response.json())
-    .then((data) =>
-      data.map((el) => ({
-        id: el.id,
-        address: el.address,
-        vendor: el.vendor,
-        coords: [parseFloat(el.latitude), parseFloat(el.longitude)],
-      }))
+function createMap(data) {
+  map = new ymaps.Map(
+    'map',
+    {
+      center: [55.753215, 37.622504],
+      zoom: 9,
+      controls: ['zoomControl'],
+    },
+    {
+      maxZoom: 17,
+    }
+  );
+
+  const objectManager = new ymaps.ObjectManager({
+    clusterize: false,
+  });
+
+  const collection = {
+    type: 'FeatureCollection',
+    features: data.map(({ id, coords, vendor }) => ({
+      type: 'Feature',
+      id,
+      geometry: {
+        type: 'Point',
+        coordinates: coords,
+      },
+      options: {
+        ...defaultPreset,
+        iconLayout: 'default#image',
+        iconImageHref: `assets/img/placemark-${getSlug(vendor)}.png`,
+      },
+    })),
+  };
+  objectManager.add(collection);
+  map.geoObjects.add(objectManager);
+
+  map.events.add('boundschange', (event) => {
+    let shopsVisible = data.filter(({ coords }) => {
+      const x = coords[0];
+      const y = coords[1];
+      const [top, bottom] = event.originalEvent.newBounds;
+      return x > top[0] && x < bottom[0] && y > top[1] && y < bottom[1];
+    });
+    $('.Menu-List').empty();
+    createMenu(shopsVisible, objectManager);
+  });
+
+  map.geoObjects.events.add('click', (event) => {
+    const id = event.get('objectId');
+    const coords = objectManager.objects.getById(id).geometry.coordinates;
+    map.setCenter(coords, 14, {
+      duration: 500,
+      timingFunction: 'ease',
+    });
+    createCard(
+      id,
+      data.find((el) => el.id === id),
+      objectManager
     );
+
+    map.geoObjects.events.add('click', (event) => {
+      if (event.get('objectId') !== id) {
+        objectManager.objects.setObjectOptions(id, { ...defaultPreset });
+      }
+    });
+  });
+
+  createMenu(data, objectManager);
+
+  $('.Map-Modal').css('display', 'block');
+}
+
+function createMenu(shops, objectManager) {
+  shops.forEach((shop) => {
+    const menuItem = getCardContent(shop);
+
+    menuItem.bind('mouseover', function () {
+      objectManager.objects.setObjectOptions(shop.id, { ...hoverPreset });
+    });
+    menuItem.bind('mouseout', function () {
+      objectManager.objects.setObjectOptions(shop.id, { ...defaultPreset });
+    });
+    menuItem.appendTo($('.Menu-List'));
+  });
+  $('.Menu-Expansion').append(
+    ` <p>Смотреть в списке <b>${shops.length} оффлайн-${pluralizeShop(shops.length)}</b></p>`
+  );
+  if (window.innerWidth < 560) {
+    $('.Menu-Item').bind('click', function (event) {
+      const id = event.currentTarget.id.replace('item-', '');
+      createCard(
+        id,
+        shops.find((el) => el.id === id),
+        objectManager
+      );
+      $('.Menu').removeClass('Expanded');
+    });
+  }
+}
+
+function getCardContent(shop) {
+  const img = $('<img>').attr('src', `assets/img/${getSlug(shop.vendor)}.png`);
+  const image = $('<div>').addClass('Menu-Item-Image').append(img);
+  const vendor = $('<h4>' + shop.vendor + '</h4>');
+  const address = $('<p>' + shop.address + '</p>');
+  const info = $('<div>').addClass('Menu-Item-Info').append(vendor, address);
+  const menuItem = $('<div>').addClass('Menu-Item').attr('id', `item-${shop.id}`).append(image, info);
+  return menuItem;
+}
+
+function createCard(id, data, objectManager) {
+  const content = $('<div>').addClass('Map-Modal-Card-Content').append(getCardContent(data));
+
+  const close = $('<span>')
+    .addClass('close-btn')
+    .bind('click', function () {
+      $('.Map-Modal-Card').css('display', 'none');
+      objectManager.objects.setObjectOptions(id, { ...defaultPreset });
+    });
+  $('.Map-Modal-Card').empty().append(content, close);
+  $('.Map-Modal-Card').css('display', 'block');
+  objectManager.objects.setObjectOptions(id, { ...hoverPreset });
 }
 
 function getSlug(vendor) {
